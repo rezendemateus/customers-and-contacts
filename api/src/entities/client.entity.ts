fa6d68ab2231f092d6ec39d8210d9967ab8d4b2e
Mateus Rezende <mateusrezende1997@hotmail.com>
2023-03-23 16:35:52 -0300

    feat: entities created

---
+++ api/src/entities/client.entity.ts
@@ -0,0 +1,54 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+  OneToMany,
+  DeleteDateColumn,
+} from "typeorm";
+import { hashSync } from "bcryptjs";
+import { Contact } from "./contact.entity";
+
+@Entity("clients")
+class Client {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  cpf: string;
+
+  @Column()
+  telephone: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = hashSync(this.password, 10);
+  }
+
+  @OneToMany(() => Contact, (contact) => contact.client)
+  contacts: Contact[];
+}
+
+export { Client };
