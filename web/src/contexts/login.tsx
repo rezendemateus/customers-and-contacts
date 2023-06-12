685ae0de35252be8bffa91558280d097bdee6106
Mateus Rezende <mateusrezende1997@hotmail.com>
2023-06-12 12:33:53 -0300

    fix: fixed redirect to login page

--- web/src/contexts/login.tsx
+++ web/src/contexts/login.tsx
@@ -1,11 +1,9 @@
 import { ILoginContext } from "@/interfaces/contexts";
 import { ILoginProvider } from "@/interfaces/providers";
 import api from "@/services/api";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext } from "react";
 import { toast } from "react-toastify";
 import Router from "next/router";
-import { ClientContext } from "./client";
-import jwt from "jsonwebtoken";
 
 export const LoginContext = createContext({} as ILoginContext);
 
